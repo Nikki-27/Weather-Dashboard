71ca2f9e49cf39e22af8642e230585fda1555be4
Nikki-27 <87594658+Nikki-27@users.noreply.github.com>
2024-05-30 19:49:37 +0530

    commit

--- src/api/weather.js
+++ src/api/weather.js
@@ -1,27 +1,18 @@
-const express = require('express');
-const axios = require('axios');
-const app = express();
-const port = 3001;
+import axios from 'axios';
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+const apiKey = process.env.WEATHER_API_KEY;
 
-app.get('/api/weather/:lat/:lon/days/:days', async (req, res) => {
-  const { lat, lon, days } = req.params;
-  const apiKey = process.env.WEATHER_API_KEY;
+const handler = async (req, res) => {
+  const { lat, lon, days } = req.query;
 
   try {
     const response = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${lat},${lon}&days=${days}`);
-    res.json(response.data);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.status(200).json(response.data);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An error occurred while fetching the weather data.' });
   }
-});
+};
 
-app.listen(port, () => {
-  console.log(`Weather proxy server listening at http://localhost:${port}`);
-});
+export default handler;
